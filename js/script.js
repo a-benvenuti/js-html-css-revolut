31cd619cb4ef6c5da1bc7b38b9ea5eb34ddea6e0
Andrea <b.a.benvenuti3@gmail.com>
2021-03-15 17:14:35 +0100

    Aggiunti eventi all'hover e al click

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-
+$(
+  function(){
+// ------------eventi all'hover-----------------
+    $('.dropdown_principale').hover(function(){
+      $('.dropdown').hide();
+      $(this).children('ul').show();
+    });
+// ------------/eventi all'hover-----------------
+//--------------eventi al click------------------
+    $(document).click(function(){
+      $(".dropdown").hide();
+    });
+//--------------/eventi al click------------------
+  }
+);
